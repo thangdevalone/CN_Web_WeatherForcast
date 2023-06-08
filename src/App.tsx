d17c39263e0439bfb1ae698396333373c7f15e10
ThangDevAlone <quangthangvtlg@gmail.com>
2023-06-09 01:16:09 +0700

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import { Box } from '@mui/material';
 import './App.css';
-import { MainRouter } from './Routers';
 
-
+import { useEffect } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { NavBar } from './utils/NavBar';
+import { DashBoardRouter } from './Routers';
+import { NewUser } from './views/NewUser';
 
 
 function App() {
+    const navigate = useNavigate();
+    const stored = localStorage.getItem('weather_app_infor');
+    const parsed = stored ? JSON.parse(stored) : null;
+    const location = useLocation();
+    useEffect(() => {
+        if (!parsed) {
+            navigate('/infor');
+        } else if (location.pathname !== '/home') {
+            navigate('/home');
+        }
+    }, [location.pathname, navigate, parsed]);
+
     return (
         <Box sx={{ position: 'relative' }}>
-            <MainRouter/>
+            <Routes>
+            {parsed && (
+                <Route
+                    path="/*"
+                    element={
+                        <>
+                            <NavBar />
+                            <DashBoardRouter />
+                        </>
+                    }
+                />
+            )}
+            <Route path="/infor" element={<NewUser />} />
+        </Routes>
         </Box>
     );
 }
