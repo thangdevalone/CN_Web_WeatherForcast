dc2e8a7db2d19937de14dfd8f9fc368711e4b10e
ThangNguyen <101063068+thangdevalone@users.noreply.github.com>
2025-05-24 11:47:16 +0700

    Update NewUser.tsx

--- src/views/NewUser.tsx
+++ src/views/NewUser.tsx
@@ -22,7 +22,6 @@ export function NewUser() {
 
     const schema = yup.object().shape({
         name: yup.string().required('Vui lòng nhập tên'),
-        avatar: yup.mixed().required('Cần tải lên ảnh đại diện'),
         location:yup.string().required('Cần nhập vị trí của bạn')
     });
 
